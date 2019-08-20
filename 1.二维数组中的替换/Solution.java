f3668d35b7112501c371517e6407359df9f552a3
Night-longs <41976483+Night-longs@users.noreply.github.com>
2019-08-20 14:26:21 +0800

    Update Solution.java

--- 1.二维数组中的替换/Solution.java
+++ 1.二维数组中的替换/Solution.java
@@ -1,19 +1,19 @@
-  public class Solution {
-        public boolean Find(int target, int[][] array) {
-            int row = array.length;
-            int clos = array[0].length;
-            int i = row - 1;
-            int j = 0;
-            while (i >= 0 && j < clos) {
-                if (target < array[i][j]) {
-                    i--;
-                } else if (target > array[i][j]) {
-                    j++;
-                } else {
-                    return true;
-                }
-
+public class Solution {
+    public boolean Find(int target, int[][] array) {
+        int row = array.length;
+        int clos = array[0].length;
+        int i = row - 1;
+        int j = 0;
+        while (i >= 0 && j < clos) {
+            if (target < array[i][j]) {
+                i--;
+            } else if (target > array[i][j]) {
+                j++;
+            } else {
+                return true;
             }
-            return false;
+
         }
+        return false;
     }
+}
